45ff9efaa442a7e9a2994ba850231383905b9c74
Zagaz <andre_ranulfo@hotmail.com>
2024-08-09 04:51:55 -0300

    chore: Update WooCommerce API endpoint, fix formatting, and add order placement functionality

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 const WOOAPI = "https://woolibresign.s1-tastewp.com/wp-json/wc/v3";
 var auth =
   "Y2tfNDQ2ZjQxYzZiOTc0MGEzYWExZWU5NmZkNzAwNmE2MDcxZDhjZDg5Mjpjc183OWMzYjkyM2ZmNGM0ZjE1MjgyYWNmODhiNDdjMWQ1NTczNTg4MDNk";
-
 var payment_method = "paypal";
 var payment_method_title = "PayPal";
 var set_paid = true;
@@ -81,34 +80,39 @@ async function post_order() {
   const order = await fetch(`${WOOAPI}/orders`, options_post);
   if (!order.ok) {
     const message = `An error has occured: ${order.status}`;
+    document.querySelector(".orderPost").classList.add("error");
+    document.querySelector(".orderGet").classList.add("error");
+    document.querySelector(".orderGet").innerHTML = message;
+    document.querySelector(".orderPost").innerHTML = message;
     throw new Error(message);
   } else {
     let res = await order.json();
     let order_id_post = res["id"];
-    console.log(`Order #${order_id_post} has been placed"`);
+    document.querySelector(".orderPost").classList.add("success");
+
+    document.querySelector(".orderPost").innerHTML = `Order #${order_id_post} successfully registered.`;
     get_order_by_ID(order_id_post);
   }
 }
 
 async function get_order_by_ID(order_id) {
-  const order = await fetch(`${WOOAPI}/orders/${order_id}`, options_get);
+  const order = await fetch(`${WOOAPI}/ordrs/${order_id}`, options_get);
   if (!order.ok) {
     const message = `An error has occured: ${order.status}`;
+    document.querySelector(".orderGet").classList.add("error");
+    document.querySelector(".orderGet").innerHTML = message;
     throw new Error(message);
   } else {
     let res = await order.json();
-    console.log(res);
     let order_id_get = res["id"];
     let first_name = res["billing"]["first_name"];
     let last_name = res["billing"]["last_name"];
     let currency = res["currency"];
-    console.log(`
-          Order #${order_id_get} has been retrieved successfully.
-               `);
-    console.log(`
-               First Name: ${first_name} 
-               Last Name: ${last_name}
-               Price: ${currency} ${res["total"]}
-               `);
+    document.querySelector(".orderGet").classList.add("success");
+    document.querySelector(".orderGet").innerHTML = `Order #${order_id_get} has been retrieved successfully.<br>
+    First Name: ${first_name}<br>
+    Last Name: ${last_name}<br>
+    Price: ${currency} ${res["total"]}`;
+  
   }
 }
